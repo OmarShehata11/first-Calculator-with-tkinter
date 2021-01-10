7a6e844f944c12af66c8cff2de4136204f513230
Omar Shehata <71821822+OmarShehata11@users.noreply.github.com>
2021-01-10 04:40:01 +0200

    adding the clear and the ANS button to the program

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ if __name__ == '__main__':
 
 
 screen = Entry(root, width=77, borderwidth=5)
-screen.grid(row=0, columnspan=4, padx=0, pady=9)
+screen.grid(row=1, columnspan=4, padx=0, pady=9)
 
 
 def addButton(num):
@@ -87,9 +87,27 @@ def num_equal():
         tempValue2 = tempValue2.split(" / ")
         screen.delete(0, END)
         screen.insert(0, float(tempValue) / float(tempValue2[-1]))
+    global ANS
+    ANS = float(screen.get())
+
+
+def clear():
+    screen.delete(0, END)
+
+
+def Ans():
+    global ANS
+    if ANS:
+        screen.delete(0, END)
+        screen.insert(0, ANS)
+
+
+button_clear = Button(root, text="Clear", padx=90, pady=10, bg="lightblue", borderwidth=3, command=clear)
+button_ANS = Button(root, text="ANS", padx=90, pady=10, bg="lightblue", borderwidth=3, command=Ans)
 
 
 button_equal = Button(root, text="=", padx=40, pady=20, bg="lightblue", highlightcolor="black", command=num_equal)
+
 
 button1 = Button(root, text="1", padx=40, pady=20, command=lambda: addButton(1))
 button2 = Button(root, text="2", padx=40, pady=20, command=lambda: addButton(2))
@@ -113,27 +131,31 @@ button_dev = Button(root, text="/", padx=90, pady=20, bg="lightgreen", command=n
 button_dot = Button(root, text=".", padx=42, pady=20, command=lambda: addButton("."))
 
 
-button_equal.grid(row=4, column=2)
+button_clear.grid(row=0, column=0, columnspan=3)
+button_ANS.grid(row=0, column=2, columnspan=6)
 
-button1.grid(row=3, column=0)
-button2.grid(row=3, column=1)
-button3.grid(row=3, column=2)
 
-button4.grid(row=2, column=0)
-button5.grid(row=2, column=1)
-button6.grid(row=2, column=2)
+button_equal.grid(row=5, column=2)
 
-button7.grid(row=1, column=0)
-button8.grid(row=1, column=1)
-button9.grid(row=1, column=2)
+button1.grid(row=4, column=0)
+button2.grid(row=4, column=1)
+button3.grid(row=4, column=2)
 
-button0.grid(row=4, column=0)
+button4.grid(row=3, column=0)
+button5.grid(row=3, column=1)
+button6.grid(row=3, column=2)
 
-button_add.grid(row=1, column=3)
-button_sub.grid(row=2, column=3)
-button_multi.grid(row=3, column=3)
-button_dev.grid(row=4, column=3)
+button7.grid(row=2, column=0)
+button8.grid(row=2, column=1)
+button9.grid(row=2, column=2)
 
-button_dot.grid(row=4, column=1)
+button0.grid(row=5, column=0)
+
+button_add.grid(row=2, column=3)
+button_sub.grid(row=3, column=3)
+button_multi.grid(row=4, column=3)
+button_dev.grid(row=5, column=3)
+
+button_dot.grid(row=5, column=1)
 
 root.mainloop()
